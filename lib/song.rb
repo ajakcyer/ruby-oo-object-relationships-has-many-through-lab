be77cfbec1b1f16fcff3777c86d47c3823578c15
ajakcyer <ajakcyer97@gmail.com>
2020-09-17 20:23:04 -0400

    done

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :namee, :artist, :genre
+
+    @@all = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
